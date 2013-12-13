639e9d4b49303cce9a519291a2bb45444f808d3b
RayAllen <ray@ray-Lenovo-IdeaPad-S400.(none)>
2013-12-13 16:15:21 +0800

    fix abbr "cams2" to "xecam" for in.php
    Signed-off-by: RayAllen <ray@ray-Lenovo-IdeaPad-S400.(none)>

--- api/in.php
+++ api/in.php
@@ -47,7 +47,7 @@ if (true || $ip == "66.180.199.11" || $ip == "127.0.0.1") {
 	$conn = new zmysqlConn();
 	$sql = "select a.*, g.companyid, b.id as 'typeid' 
 		from agent_site_mappings a, sites s, accounts n, types b, agents g, companies m 
-		where a.siteid = s.id and a.siteid = b.siteid and s.abbr = 'cams2' 
+		where a.siteid = s.id and a.siteid = b.siteid and s.abbr = 'xecam' 
 			and a.agentid = g.id and g.companyid = m.id
 			and a.agentid = n.id and n.username = '$agent'
 		ORDER BY typeid";
